65abf9e7207bf9414553cf41e744222e652049fa
echao <echao8@uwo.ca>
2024-01-25 13:25:47 -0500

    NavBar setup in progress

---
+++ my-app/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../components/NavBar'
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>CHEER</h1>
+      </header>
+      <main>
+        <p>Welcome to CHEER! This is a basic example of a React project.</p>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
