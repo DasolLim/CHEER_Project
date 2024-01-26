d55ef9f6c1018d85b49c652e0452e62804c1d970
ansonruan7 <ansonruan4@gmail.com>
2024-01-25 20:48:00 -0500

    added database and collection name

--- my-app/src/Backend/backend.js
+++ my-app/src/Backend/backend.js
@@ -21,6 +21,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+//Checking that a connection can be formed
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,7 +34,12 @@ async function run() {
     await client.close();
   }
 }
+//Run the connection check
 run().catch(console.dir);
+
+//Database of users
+const db = client.db(CHEER);
+const users = db.collection('users');
 
 //Assign a port
 let port = 3000;
