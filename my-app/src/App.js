996fee042803bab26d0d2b96d7407827785f0c31
echao <echao8@uwo.ca>
2024-01-28 16:06:28 -0500

    Navbar deisgn v1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import HomePage from './pages/HomePage';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar/Navbar';
+
+// Different Roboto font weights for Material UI
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <HomePage />
+    <div className='App'>
+      <Navbar/>
     </div>
   );
 }
---
+++ my-app/src/components/Navbar/consts/navbarItems.js
@@ -0,0 +1,45 @@
+import FaceIcon from '@mui/icons-material/Face';
+import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
+import NewspaperRoundedIcon from '@mui/icons-material/NewspaperRounded';
+import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
+import Diversity3RoundedIcon from '@mui/icons-material/Diversity3Rounded';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+
+export const mainNavbarItems = [
+    {
+        id: 0,
+        icon: <FaceIcon />,
+        label: 'Authentication',
+        route: 'authentication',
+    },
+    {
+        id: 1,
+        icon: <CalendarMonthRoundedIcon />,
+        label: 'Calender',
+        route: 'calender',
+    },
+    {
+        id: 2,
+        icon: <NewspaperRoundedIcon />,
+        label: 'Newsletter',
+        route: 'newsletter',
+    },
+    {
+        id: 3,
+        icon: <ChatRoundedIcon />,
+        label: 'Chat',
+        route: 'chat',
+    },
+    {
+        id: 4,
+        icon: <Diversity3RoundedIcon />,
+        label: 'Friends',
+        route: 'friends',
+    },
+    {
+        id: 5,
+        icon: <InfoRoundedIcon />,
+        label: 'Info',
+        route: 'info',
+    },
+]
