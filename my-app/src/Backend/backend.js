8ca4e8f4d56b39f6b2e1feac18fbb4996b6c8ee1
ansonruan7 <ansonruan4@gmail.com>
2024-01-30 17:24:45 -0500

    Insert and delete functionality

--- my-app/src/Backend/backend.js
+++ my-app/src/Backend/backend.js
@@ -2,6 +2,17 @@
 const express = require('express');
 const app = express();
 
+//Set routers
+const router_users = express.Router();
+router_users.use(express.json());
+app.use('/api/users',router_users);
+
+//Setup middleware to do logging
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+})
+
 //Get mongoose module
 // const mongoose = require('mongoose');
 
@@ -38,9 +49,53 @@ async function run() {
 run().catch(console.dir);
 
 //Database of users
-const db = client.db(CHEER);
+const db = client.db("CHEER");
 const users = db.collection('users');
 
 //Assign a port
 let port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//USERS FUNCTIONALITIES ------------------------------------------------------------------------------------------------------------------------------
+//Register new parent user
+router_users.post('/register', async (req, res) => {
+  try {
+    //Open client
+    await client.connect();
+    //Get login information from HTML body
+    const login = req.body;
+    //Create a document to be inserted
+    const doc = {
+      username: login.username,
+      password: login.password,
+      email: login.email,
+      type: "Parent",
+      accounts: [],
+    }
+    //Insert into database
+    await users.insertOne(doc);
+    //Send status response
+    res.status(200).send('Registration Complete');
+  } catch (error) {
+    //Send status response
+    res.status(400).send('Bad Request');
+  } finally {
+    //Close client
+    await client.close();
+  }
+});
+
+//Delete a parent user
+router_users.delete('/delete', async (req, res) => {
+  try {
+    await client.connect();
+    const username = req.body;
+    console.log(username);
+    await users.deleteOne(username);
+    res.status(200).send('Deletion Successful');
+  } catch (error) {
+    res.status(404).send('User not found');
+  } finally {
+    await client.close();
+  }
+})
