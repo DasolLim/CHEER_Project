785bf5850673b9cc8f3433fc908568740c56ff1a
aruan4 <aruan4@uwo.ca>
2024-02-01 18:08:14 -0500

    Started building an example newsletter

---
+++ my-app/src/pages/Newsletter/News _Pages/Jan_news.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../../index.css';
+import temp from '../../../Assets/SE3350.jpg'
+
+const Jan_news = () => {
+    return(
+        <div className='newsletter_container'>
+            <div className='newsletter_image'>
+                <img src={temp} class='standard' alt='Newsletter Object'></img>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam nisl, ut aliquet ligula. Integer a congue libero. Vivamus a scelerisque massa, ut sodales ante. Suspendisse vehicula ornare urna, in scelerisque ipsum aliquet vel. Mauris pulvinar finibus nunc. Sed faucibus est et eros faucibus aliquet. Nullam at facilisis ex. Fusce vehicula mattis lorem, at malesuada nisi eleifend sed. Cras cursus felis eleifend velit imperdiet interdum. Curabitur vulputate sem sapien, et tristique elit efficitur nec. Duis ante dui, suscipit facilisis tincidunt sit amet, vestibulum quis erat. Nulla mattis lacus vel eros hendrerit dictum. Nullam facilisis iaculis nulla nec lobortis. Curabitur eu odio a magna posuere sollicitudin. Integer aliquam dui non dolor sollicitudin, at ullamcorper ante interdum.
+
+Sed ex arcu, blandit eget metus ut, viverra mollis sapien. Ut sit amet lorem velit. Proin maximus sodales nunc sit amet placerat. Nullam velit ipsum, laoreet at pretium in, accumsan viverra est. Nam eget lobortis metus. Nunc maximus vitae urna lacinia tempor. Donec nisl risus, sodales vitae arcu quis, aliquam tempor mauris. Pellentesque eu sagittis augue. In et nibh ac nulla mattis dictum. Etiam feugiat ante gravida eros dictum ornare. Vivamus nec maximus lacus. Mauris id metus nec magna condimentum scelerisque. Vivamus ut orci et ligula faucibus consectetur.
+
+Quisque non vulputate ex, sed fermentum nisi. Nullam et tincidunt erat. Vivamus accumsan cursus odio, nec convallis justo. Sed bibendum, orci ac rutrum eleifend, odio massa interdum risus, sed tincidunt est ex eu est. Sed porttitor ac felis et fringilla. In vitae dapibus metus, non aliquam arcu. Vivamus neque lacus, volutpat sed mauris ac, tincidunt hendrerit quam. Curabitur accumsan enim at lacus mollis gravida. Curabitur a mauris neque. Curabitur neque ante, gravida bibendum velit nec, tempus mattis nunc. Maecenas eget ultricies neque. Suspendisse orci magna, dapibus at aliquam ut, tincidunt nec sem. Phasellus consectetur risus quis metus faucibus faucibus. Aliquam hendrerit eleifend orci vitae elementum.
+
+Etiam vel facilisis odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ullamcorper, augue nec vestibulum ornare, est enim elementum magna, vel aliquet augue velit et dui. Curabitur nec turpis mollis, mollis mauris eu, dignissim purus. Vestibulum luctus mauris non mauris tempus, ornare blandit enim vehicula. Donec iaculis porttitor tincidunt. Integer mollis pulvinar purus, quis lacinia magna porttitor ac. Suspendisse luctus porta varius. Phasellus aliquet risus vel lectus suscipit pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam feugiat orci sit amet tincidunt lacinia. Etiam iaculis urna et pulvinar efficitur. Nulla vitae neque non mi mattis euismod non in massa. In dignissim odio et commodo varius. Proin egestas vulputate lacus, sed luctus metus interdum et. Aenean egestas eros a magna vulputate, et aliquet mauris accumsan.
+
+Quisque lacus tellus, convallis eu pulvinar ullamcorper, convallis sed quam. Curabitur ut arcu purus. Ut rutrum elementum libero. Suspendisse at dignissim tortor, et interdum tellus. Maecenas lacinia fringilla pretium. Nunc vestibulum eleifend mattis. Curabitur nibh odio, euismod suscipit porttitor sit amet, dapibus id nibh. Ut imperdiet sed justo eu tempus.</p>
+        </div>
+    );
+}
+
+export default Jan_news;
