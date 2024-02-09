e7e3182f7ab602f4d80ece181fd527ce0e88b38e
aruan4 <aruan4@uwo.ca>
2024-02-08 22:18:30 -0500

    Partial update of the homepage

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-// import CommonButton from '../../components/Navbar/common/CommonButton/CommonButton';
-import { Container } from '@mui/material';
-// import { useLocation } from 'react-router-dom'
-import joyfulBackground from '../../Assets/JoyfulBackground.jpg'
+import OLLI from '../../Assets/OLLI LOGO.png';
+import CHEER from '../../Assets/cheerconnections.png';
 
 function Home() {
     // const location = useLocation()
@@ -17,30 +14,50 @@ function Home() {
     //     },
     // }
     return (
-        <Container>
-            <Grid styles={{ backgroundColor: '#FFA500' }}>
-                <h1>Welcome !!!</h1>
-                <img className='welcomeImg' src={joyfulBackground} alt="Joyful Background" />
-                <div>
-                    <p className='homeAbout'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    </p>
-                </div>
-            </Grid>
-        </Container>
+        <>
+        <nav class="nav-container">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+
+        <div className='grid grid-rows-4 gap-y-0.1 max-w-[800px] justify-center flex-col text-center items-center mx-auto'>
+            <div class="button-container">
+                <button id="btn1">Button 1</button>
+                <button id="btn2">Button 2</button>
+                <button id="btn3">Button 3</button>
+            </div>
+
+            <div id='row1' className='italic p-[-5]'>
+                <p>Vision Statement—
+                    To be a community of inclusion and
+                    a circle of friendship that supports
+                    and enhances the lives of our loved
+                    ones with intellectual disabilities as
+                    well as the whole family.
+                </p>
+            </div>
+            <div id='row2' className='flex items-center justify-end m-6 px-12'>
+                <img src={OLLI} className='w-[25%] mx-12'></img>
+                <p>OLLI is a registered not-for-profit
+                    caregiver driven company with
+                    four areas of focus: Cheer Group;
+                    Cheer Works; Cheer Connections;
+                    and, Cheer Living.
+                </p>
+            </div>
+            <div id='row3' className='flex items-center justify-start m-6 px-12'>
+                <p>Caregiver social and support
+                    group, creators and administrators
+                    of all things C.H.E.E.R.
+                </p>
+                <img src={CHEER} className='w-[35%] mx12'></img>
+            </div>
+        </div>
+        </>
     )
 }
 
