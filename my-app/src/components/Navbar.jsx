f7544c6c947204d1ec504d84b339ceb181d482e9
aruan4 <aruan4@uwo.ca>
2024-02-09 11:33:46 -0500

    completed theme and home page changes

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import OLLI from '../Assets/OLLI LOGO.png';
 
 const Navbar = () => {
     return(
         <div>
             <div className='bg-[#FFF] w-full align-center'>
                 <div>
-                    <div className='w-[235px]'>
-                        <p className='text-[#DB7859] text-[32px] font-extrabold'>OLLI NETWORK</p>
-                    </div>
-                    <ul className='list-none flex justify-end py-8 px-20'>
-                        <li className='inline'>
-                            <a className='inline-block text-[#3A3A3A] text-[24px] font-extrabold px-6' href="#">Home</a>
+                    <ul className='list-none flex justify-end py-[-1%] px-20'>
+                        <li className='flex'>
+                            <img className='scale-[25%] mx-[-35%] my-[-30%]' src={OLLI}></img>
+                            <p className='text-[#DB7859] text-[24px] font-extrabold my-auto w-[100px]'>OLLI NETWORK</p>
                         </li>
-                        <li className='inline'>
-                            <a className='inline-block text-[#3A3A3A] text-[24px] font-extrabold px-8' href="#">About</a>
+                        <li className='inline my-auto'>
+                            <a className='inline-block text-[#3A3A3A] text-[24px] font-extrabold px-6' href="/">Home</a>
                         </li>
-                        <li className='inline'>
+                        <li className='inline my-auto'>
                             <a className='inline-block text-[#3A3A3A] text-[24px] font-extrabold px-8' href="#">Services</a>
                         </li>
-                        <li className='inline'>
-                            <a className='inline-block text-[#3A3A3A] text-[24px] font-extrabold px-8' href="#">Contact</a>
+                        <li className='inline my-auto'>
+                            <a className='inline-block text-[#3A3A3A] text-[24px] font-extrabold px-8' href="newsletter">Newsletters</a>
                         </li>
-                        <li className='inline'>
-                            <a className='inline-block text-[#647249] text-[24px] font-extrabold px-8' href="#">REGISTER</a>
+                        <li className='inline my-auto'>
+                            <a className='inline-block text-[#647249] text-[24px] font-extrabold px-8' href="login">LOGIN</a>
+                        </li>
+                        <li className='inline my-auto'>
+                            <a className='inline-block text-[#647249] text-[24px] font-extrabold px-8' href="register">REGISTER</a>
                         </li>
                     </ul>
                 </div>
--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -14,41 +14,53 @@ function Home() {
     //     },
     // }
     return (
-        <>
-        <div className='grid grid-rows-4 gap-y-0.1 max-w-[800px] justify-center flex-col text-center items-center mx-auto'>
-            <div class="button-container">
-                <button id="btn1">Button 1</button>
-                <button id="btn2">Button 2</button>
-                <button id="btn3">Button 3</button>
+        <div className='grid grid-cols-2 items-center justify-center text-center'>
+            <div className='items-center my-auto'>
+                <div className="flex m-auto justify-between flex-col w-[75%]">
+                    <p className='w-inherit text-[#DB7859] text-[60px] font-semibold'>Welcome to the OLLI NETWORK</p>
+                    <a href='info'>
+                        <button className='text-white font-semibold w-full inline-flex my-4 justify-center py-[10px] px-[82px] rounded-md shadow-md bg-[#498184]
+                        hover:scale-[102.5%] ease-in duration-75 hover:bg-[#6daeb1]'>Program and Events</button>
+                    </a>
+                    <a href='calendar'>
+                        <button className='text-white font-semibold w-full inline-flex my-4 justify-center py-[10px] px-[82px] rounded-md shadow-md bg-[#EE807E]
+                        hover:scale-[102.5%] ease-in duration-75 hover:bg-[#ea8f8d]'>Calendar</button>
+                    </a>
+                    <a href='calendar'>
+                        <button className='text-white font-semibold w-full inline-flex my-4 justify-center py-[10px] px-[82px] rounded-md shadow-md bg-[#8D617C]
+                        hover:scale-[102.5%] ease-in duration-75 hover:bg-[#8d6a7f]'>Login</button>
+                    </a>
+                </div>
             </div>
-
-            <div id='row1' className='italic p-[-5]'>
-                <p>Vision Statement—
-                    To be a community of inclusion and
-                    a circle of friendship that supports
-                    and enhances the lives of our loved
-                    ones with intellectual disabilities as
-                    well as the whole family.
-                </p>
-            </div>
-            <div id='row2' className='flex items-center justify-end m-6 px-12'>
-                <img src={OLLI} className='w-[25%] mx-12'></img>
-                <p>OLLI is a registered not-for-profit
-                    caregiver driven company with
-                    four areas of focus: Cheer Group;
-                    Cheer Works; Cheer Connections;
-                    and, Cheer Living.
-                </p>
-            </div>
-            <div id='row3' className='flex items-center justify-start m-6 px-12'>
-                <p>Caregiver social and support
-                    group, creators and administrators
-                    of all things C.H.E.E.R.
-                </p>
-                <img src={CHEER} className='w-[35%] mx12'></img>
+            <div className='m-auto p-auto'>
+                <br></br><br></br>
+                <div id='row1' className='italic my-auto'>
+                    <p>Vision Statement—
+                        To be a community of inclusion and
+                        a circle of friendship that supports
+                        and enhances the lives of our loved
+                        ones with intellectual disabilities as
+                        well as the whole family.
+                    </p>
+                </div>
+                <div id='row2' className='flex items-center justify-end m-6 px-12'>
+                    <img src={OLLI} className='w-[25%] mx-12'></img>
+                    <p>OLLI is a registered not-for-profit
+                        caregiver driven company with
+                        four areas of focus: Cheer Group;
+                        Cheer Works; Cheer Connections;
+                        and, Cheer Living.
+                    </p>
+                </div>
+                <div id='row3' className='flex items-center justify-start m-6 px-12'>
+                    <p>Caregiver social and support
+                        group, creators and administrators
+                        of all things C.H.E.E.R.
+                    </p>
+                    <img src={CHEER} className='w-[35%] mx12'></img>
+                </div>
             </div>
         </div>
-        </>
     )
 }
 
